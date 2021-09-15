339db0decb75dcf757d6c978557860a28677e651
Krivtsov Pavel <pkrivtsov6@gmail.com>
2021-09-15 19:44:44 +0300

    added func to find Multiplicative Inverse

--- CipherApp/EncryptionAlgorithms/RSA.swift
+++ CipherApp/EncryptionAlgorithms/RSA.swift
@@ -18,10 +18,41 @@ class RSA {
         ((0, 0), (0, 0))
     }
     
-    private func multiplicativeInverse(for e: Int, and phi: Int) -> Int {
-        0
+    // Euclid's extended algorithm for finding the multiplicative inverse of two numbers.
+    // In other words, we are looking for d such that d * e mod phi = 1
+    private func findMultiplicativeInverse(for e: Int, and phi: Int) -> Int {
+        let phiInitial = phi
+        
+        // values ​​from formula
+        var e = e
+        var phi = phi
+        var y = 1
+        var d = 0
+        var i = -1
+        
+        var integerParts: [Int] = []
+        
+        while e % phi != 0 {
+            i += 1
+            integerParts.append(e / phi)
+            
+            let remainder = e % phi
+            e = phi
+            phi = remainder
+        }
+        
+        for i in (0...i).reversed() {
+            (d, y) = (y, d - y * integerParts[i])
+        }
+        
+        if d < 0 {
+            d += phiInitial
+        }
+        
+        return d
     }
     
+    // Euclid's algorithm for determining the greatest common divisor.
     private func findGreatestCommonDivisor(for x: Int, and y: Int) -> Int {
         
         if x == 0 || y == 0 {
