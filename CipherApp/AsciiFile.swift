83e5835fcb119332e411fae108e5b793e329ac0a
Nikita12G <gulyi08@mail.ru>
2021-09-19 16:18:09 +0700

    key casting

--- CipherApp/AsciiFile.swift
+++ CipherApp/AsciiFile.swift
@@ -11,30 +11,31 @@ import Foundation
 
 class Ascii {
     
-    var keyFoAsciiTable = 10
+    let asciiTableCount = 127
+    var keyFoAsciiTable = 100
     
-    func encryption(_ encryptedValue: String) -> String {
+    func encryption() -> String {
         
         let encryptedValue = "Swift"
         var encryption = ""
         
         for cipher in encryptedValue {
-            let asciiCode = (Int((cipher.asciiValue ?? 0 )) + keyFoAsciiTable) % 127
-            encryption.append(Character(UnicodeScalar(asciiCode)!))
+            let asciiCode = (Int((cipher.asciiValue ?? 0 )) + keyFoAsciiTable) % asciiTableCount
+            encryption.append(Character(UnicodeScalar(asciiCode) ?? " "))
         }
         return encryption
     }
     
     //Метод для расшифровки
     
-    
-    func decrypt(_ decryptionValue: String) -> String {
+    func decrypt() -> String {
         
         let decryptionValue = "Vzliw"
         var decryption = ""
         
         for decrypt in decryptionValue {
-            decryption.append(Character(UnicodeScalar((decrypt.asciiValue ?? 0 ) - UInt8(keyFoAsciiTable))))
+            let asciiCode = (Int((decrypt.asciiValue ?? 0 ) - UInt8(keyFoAsciiTable)) + asciiTableCount) % asciiTableCount
+            decryption.append(Character(UnicodeScalar(asciiCode) ?? " "))
         }
         return decryption
     }
